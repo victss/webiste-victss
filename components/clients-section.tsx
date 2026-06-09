b422b4798c08eea1730c6f04fb80ea8bdae6e6fc
victss <victss.info@gmail.com>
2026-06-09 22:48:21 +0530

    added-new-partners

--- components/clients-section.tsx
+++ components/clients-section.tsx
@@ -1,6 +1,24 @@
 import { Card, CardContent } from "@/components/ui/card";
 
 export function ClientsSection() {
+  const partners = [
+    { id: 1, logo: "https://www.drupal.org/files/project-images/Logo%20ONLYOFFICE.png", alt: "ONLYOFFICE", website: "https://www.onlyoffice.com" },
+    { id: 2, logo: "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png", alt: "AWS", website: "https://aws.amazon.com" },
+    { id: 3, logo: "/adobe.svg", alt: "Adobe", website: "https://www.adobe.com" },
+    { id: 4, logo: "/math.png", alt: "Wiris MathType", website: "https://www.wiris.com/en/" },
+    { id: 5, logo: "/smart.png", alt: "SmartPLS", website: "https://www.smartpls.com/" },
+    { id: 6, logo: "/asld.png", alt: "ASLD", website: "https://www.asldweb.com" },
+    { id: 7, logo: "https://offsquare.com.my/wp-content/uploads/2017/11/Microsoft-Logo-icon-png-Transparent-Background.png", alt: "Microsoft", website: "https://www.microsoft.com" },
+    { id: 8, logo: "https://www.checkpoint.com/wp-content/themes/checkpoint-theme-v2/images/checkpoint-logo.svg", alt: "Check Point", website: "https://www.checkpoint.com" },
+    { id: 9, logo: "https://mma.prnewswire.com/media/2660251/Vertiv_Corporation_Logo.jpg", alt: "Vertiv", website: "https://www.vertiv.com" },
+    { id: 10, logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVVfycNuwGheNMUskHToQCav2CNL3K9_Htlg&s", alt: "Nitro Software", website: "https://www.gonitro.com" },
+    { id: 11, logo: "https://i.pinimg.com/originals/8a/1d/0d/8a1d0dfc30186de9e6b2ae0bdea574c6.png", alt: "ServiceNow", website: "https://www.servicenow.com" },
+    { id: 12, logo: "/numerical-analytics.svg", alt: "Numerical Analytics", website: "https://www.numerical-analytics.com/" },
+    { id: 13, logo: "https://www.datocms-assets.com/40521/1726820725-login-page-thumbnail.png", alt: "greytHR", website: "https://www.greythr.com/" },
+    // { id: 14, logo: "/synapse.svg", alt: "Synapse Online", website: "https://www.synapseonline.in/" },
+    { id: 15, logo: "/techdata.svg", alt: "Tech Data", website: "https://www.techdata.com" }
+  ];
+
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-950">
       <div className="container px-4 md:px-6">
@@ -23,18 +41,12 @@ export function ClientsSection() {
           {/* Auto-Scrolling Logos */}
           <div className="flex overflow-hidden">
             <div className="flex animate-scroll space-x-6">
-            {[
-  { id: 1, logo: "https://www.drupal.org/files/project-images/Logo%20ONLYOFFICE.png", alt: "Client 1", website: "https://www.onlyoffice.com" },
-  { id: 2, logo: "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png", alt: "Client 2", website: "https://aws.amazon.com" },
-  { id: 3, logo: "https://www.adobe.com/cc-shared/assets/img/social-media-logos/creativecloud-social.jpg", alt: "Client 3", website: "https://www.adobe.com" },
-  { id: 4, logo: "math.png", alt: "Client 4", website: "https://www.wiris.com/en/" },
-  { id: 5, logo: "smart.png", alt: "Client 5", website: "https://www.smartpls.com/" },
-  { id: 6, logo: "asld.png", alt: "Client 6", website: "https://www.asldweb.com" },
-  { id: 7, logo: "https://pngimg.com/d/microsoft_PNG6.png", alt: "Client 7", website: "https://www.microsoft.com" },
-].map((client) => (
-  <ClientCard key={client.id} logo={client.logo} alt={client.alt} website={client.website} />
-))}
-
+              {partners.map((client) => (
+                <ClientCard key={client.id} logo={client.logo} alt={client.alt} website={client.website} />
+              ))}
+              {partners.map((client) => (
+                <ClientCard key={`dup-${client.id}`} logo={client.logo} alt={client.alt} website={client.website} />
+              ))}
             </div>
           </div>
         </div>
